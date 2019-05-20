74928d6e5ca83892aae810d00c99b8dea562fd95
Benetu Gschwend <benetu.gschwend@2sic.com>
2019-05-20 13:42:14 +0200

    Add Components to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppListComponent } from './app-list/app-list.component';
+import { AppListItemComponent } from './app-list/app-list-item/app-list-item.component';
+import { FilterOptionsComponent } from './filter-options/filter-options.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppListComponent,
+    AppListItemComponent,
+    FilterOptionsComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule,
