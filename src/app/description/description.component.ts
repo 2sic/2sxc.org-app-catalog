d4699260e3671f08cf4cadc9b9613241b03d9ab9
Benetu Gschwend <benetu.gschwend@2sic.com>
2019-05-20 13:40:25 +0200

    Add Description Component

---
+++ src/app/description/description.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-description',
+  templateUrl: './description.component.html',
+  styleUrls: ['./description.component.scss']
+})
+export class DescriptionComponent implements OnInit {
+
+  public title: string = null;
+  public description: string = null;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'App Catalog';
+    this.description = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est';
+  }
+
+}
