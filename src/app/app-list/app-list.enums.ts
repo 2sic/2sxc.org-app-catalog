470a12017c6db04c45d9e88a47cdae6154e048e9
Benetu Gschwend <benetu.gschwend@2sic.com>
2019-05-23 14:22:44 +0200

    Add AppList interfaces & enums

---
+++ src/app/app-list/app-list.enums.ts
@@ -0,0 +1,6 @@
+
+export enum AppTypeIds {
+  'top' = 7083,
+  'stable' = 2004,
+  'old' = 12328,
+}
---
+++ src/app/app-list/app-list.interfaces.ts
@@ -0,0 +1,16 @@
+
+export interface AppListItemTag {
+  Id: number;
+  Title: string;
+  Tag: string;
+  Category: string;
+}
+
+export interface AppListItem {
+  Id: number;
+  Type: AppListItemTag;
+  Icon: string;
+  Name: string;
+  Description: string;
+  Tags: Array<AppListItemTag>;
+}
