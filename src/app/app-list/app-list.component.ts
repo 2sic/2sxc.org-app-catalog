e15077366499878eb2bdc164b5e563074ee67aca
Benetu Gschwend <benetu.gschwend@2sic.com>
2019-05-23 14:23:54 +0200

    Changes on AppList component

--- src/app/app-list/app-list.component.ts
+++ src/app/app-list/app-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { DataService } from '../data-service/data.service';
+import { map } from 'rxjs/operators';
+import { AppListItem } from './app-list.interfaces';
+import { FilterOptionsService } from '../filter-options/fiter-options.services';
+import { AppTypeIds } from './app-list.enums';
 
 @Component({
   selector: 'app-list',
@@ -9,12 +12,53 @@ import { DataService } from '../data-service/data.service';
 })
 export class AppListComponent implements OnInit {
 
-  public appList: Observable<any> = null;
+  public appList: Observable<AppListItem[]> = null;
 
-  constructor(private data: DataService) {}
+  constructor(private filterService: FilterOptionsService) {}
 
   ngOnInit() {
-    this.appList = this.data.getAppList();
-  }
+    const addType = (apps: AppListItem[]) => {
+      return apps
+        .map(app => {
+          const hasTop = app.Tags.find(tag => tag.Id === AppTypeIds.top);
+          if (!!hasTop) {
+            app.Type = hasTop;
+            return app;
+          }
 
+          const hasStable = app.Tags.find(tag => tag.Id === AppTypeIds.stable);
+          if (!!hasStable) {
+            app.Type = hasStable;
+            return app;
+          }
+
+          const hasOld = app.Tags.find(tag => tag.Id === AppTypeIds.old);
+          if (!!hasOld) {
+            app.Type = hasOld;
+            return app;
+          }
+
+          return app;
+        });
+    };
+
+    const sortByType = (apps: AppListItem[]) => {
+      return apps
+        .map((app: AppListItem, index: number) => {
+          const sortValue = !app.Type ? 3
+            : app.Type.Id === AppTypeIds.top ? 1
+              : app.Type.Id === AppTypeIds.stable ? 2
+                : 3;
+          return { index, sortValue };
+        })
+        .sort( (a, b) => a.sortValue - b.sortValue )
+        .map( sortItem => apps[sortItem.index] );
+    };
+
+    this.appList = this.filterService.appListFiltered
+      .pipe(
+        map((appList: AppListItem[]) => addType(appList)),
+        map((appList: AppListItem[]) => sortByType(appList)),
+      );
+  }
 }
